b74ac8383f2fc00186fe532bcdab8e5be8035037
Jake Lee <jake.lee@fdmgroup.com>
2021-09-22 18:35:51 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    num = int(input())
+    print_until_number(num)
+
+
+def print_until_number(number):
+    i = 1
+    while (i <= number):
+        print(i)
+        i += 1
 
 if __name__ == '__main__':
     main()
